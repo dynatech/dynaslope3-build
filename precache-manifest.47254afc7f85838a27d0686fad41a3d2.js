80144dd8ccb5d603b13951f4d5e7ea78bb1077cd
ksdelacruz <kevindhaledelacruz@gmail.com>
2020-09-10 11:29:45 +0800

    - transition back to local

---
+++ precache-manifest.47254afc7f85838a27d0686fad41a3d2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9e383ad6cfbe752d58ac2c64ff8379",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2739c440aeb7e177f5d",
+    "url": "/static/css/2.5aeb6a47.chunk.css"
+  },
+  {
+    "revision": "e216567ddb44f5799287",
+    "url": "/static/css/main.7d52d26b.chunk.css"
+  },
+  {
+    "revision": "b2739c440aeb7e177f5d",
+    "url": "/static/js/2.f5b27887.chunk.js"
+  },
+  {
+    "revision": "13d6d7ebd89d53af4f04cb2c6813a394",
+    "url": "/static/js/2.f5b27887.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e216567ddb44f5799287",
+    "url": "/static/js/main.d184e631.chunk.js"
+  },
+  {
+    "revision": "4750c3da2a39f49c563d",
+    "url": "/static/js/runtime-main.7dbf1ffa.js"
+  },
+  {
+    "revision": "d9a91c1fd165ea9b24187b68dd521687",
+    "url": "/static/media/dyna_wall.d9a91c1f.jpg"
+  },
+  {
+    "revision": "6bf928005bc127db4515f0ce55343236",
+    "url": "/static/media/login-bg-img.6bf92800.jpg"
+  },
+  {
+    "revision": "de612ba134e8827b34fa62a1123230b3",
+    "url": "/static/media/phivolcs-dynaslope-logo.de612ba1.png"
+  },
+  {
+    "revision": "131de21276ac8cddccc775e7610070d2",
+    "url": "/static/media/phivolcs-letter-footer.131de212.png"
+  },
+  {
+    "revision": "234377b70b0e07f8f8f90cf4d81bc047",
+    "url": "/static/media/phivolcs-letter-head.234377b7.png"
+  }
+]);
