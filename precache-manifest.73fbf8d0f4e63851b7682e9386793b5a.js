3ec469b6f08e31131548af7e4c172b07b7ff067d
John David Guevarra <david063095@gmail.com>
2021-05-25 15:52:56 +0800

    initial staging build files

---
+++ precache-manifest.73fbf8d0f4e63851b7682e9386793b5a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "105bd87d3d83f575bd66897d18b04d13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6fd308288ec6cbbf46aa",
+    "url": "/static/css/2.8344305e.chunk.css"
+  },
+  {
+    "revision": "dbf9280375cdeca90eec",
+    "url": "/static/css/main.a57204dd.chunk.css"
+  },
+  {
+    "revision": "6fd308288ec6cbbf46aa",
+    "url": "/static/js/2.13dfa2d8.chunk.js"
+  },
+  {
+    "revision": "3bce6221e4e39207535801127d55ff87",
+    "url": "/static/js/2.13dfa2d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbf9280375cdeca90eec",
+    "url": "/static/js/main.b9417f2d.chunk.js"
+  },
+  {
+    "revision": "4750c3da2a39f49c563d",
+    "url": "/static/js/runtime-main.7dbf1ffa.js"
+  },
+  {
+    "revision": "d9a91c1fd165ea9b24187b68dd521687",
+    "url": "/static/media/dyna_wall.d9a91c1f.jpg"
+  },
+  {
+    "revision": "6bf928005bc127db4515f0ce55343236",
+    "url": "/static/media/login-bg-img.6bf92800.jpg"
+  },
+  {
+    "revision": "de612ba134e8827b34fa62a1123230b3",
+    "url": "/static/media/phivolcs-dynaslope-logo.de612ba1.png"
+  },
+  {
+    "revision": "131de21276ac8cddccc775e7610070d2",
+    "url": "/static/media/phivolcs-letter-footer.131de212.png"
+  },
+  {
+    "revision": "990f0dc95d58e458736bf9c4554fcd25",
+    "url": "/static/media/phivolcs-letter-head.990f0dc9.png"
+  }
+]);
