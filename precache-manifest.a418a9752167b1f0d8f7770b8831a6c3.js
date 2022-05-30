bf0753d0855384635f56673c921966febbde7e94
John David Guevarra <david063095@gmail.com>
2022-05-30 09:18:35 +0800

    may 30 update

---
+++ precache-manifest.a418a9752167b1f0d8f7770b8831a6c3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c5dc67fcb40e2fb6b46e2747b659042a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b0fe8e31a057390200ef",
+    "url": "/static/css/2.b5bbc214.chunk.css"
+  },
+  {
+    "revision": "adb3945a87d3612588c6",
+    "url": "/static/css/main.3645fea2.chunk.css"
+  },
+  {
+    "revision": "b0fe8e31a057390200ef",
+    "url": "/static/js/2.7b084fa2.chunk.js"
+  },
+  {
+    "revision": "3bce6221e4e39207535801127d55ff87",
+    "url": "/static/js/2.7b084fa2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adb3945a87d3612588c6",
+    "url": "/static/js/main.2aebea71.chunk.js"
+  },
+  {
+    "revision": "4750c3da2a39f49c563d",
+    "url": "/static/js/runtime-main.7dbf1ffa.js"
+  },
+  {
+    "revision": "d9a91c1fd165ea9b24187b68dd521687",
+    "url": "/static/media/dyna_wall.d9a91c1f.jpg"
+  },
+  {
+    "revision": "6bf928005bc127db4515f0ce55343236",
+    "url": "/static/media/login-bg-img.6bf92800.jpg"
+  },
+  {
+    "revision": "4fc52fe1693c02cf7c6baffa8cad9638",
+    "url": "/static/media/notif_sound.4fc52fe1.mp3"
+  },
+  {
+    "revision": "de612ba134e8827b34fa62a1123230b3",
+    "url": "/static/media/phivolcs-dynaslope-logo.de612ba1.png"
+  },
+  {
+    "revision": "131de21276ac8cddccc775e7610070d2",
+    "url": "/static/media/phivolcs-letter-footer.131de212.png"
+  },
+  {
+    "revision": "990f0dc95d58e458736bf9c4554fcd25",
+    "url": "/static/media/phivolcs-letter-head.990f0dc9.png"
+  }
+]);
